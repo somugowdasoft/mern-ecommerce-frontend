aa50a1e85e754ab54cbcbf3ab808f5bc8f91995e
Somu Gowda <somugowda.work@gmail.com>
2025-02-24 21:07:47 +0530

    render base ulr

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:8000"
+export const API_BASE_URL = "https://mern-ecommerce-backend-3hci.onrender.com"
 export const api = axios.create({
     baseURL: API_BASE_URL,
 });
